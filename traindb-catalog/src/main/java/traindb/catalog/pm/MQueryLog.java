dc78d5d0775bfa77086db576a82b2ebe273167d8
hagia <hagia@realtimetech.co.kr>
2022-11-24 11:00:22 +0900

    Add : Table for Query Log

---
+++ traindb-catalog/src/main/java/traindb/catalog/pm/MQueryLog.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package traindb.catalog.pm;
+
+import traindb.catalog.CatalogConstants;
+
+import javax.jdo.annotations.*;
+
+@PersistenceCapable
+public final class MQueryLog {
+  @Persistent
+  @Column(length = 32)
+  private String start;
+
+  @Persistent
+  @Unique(name = "USER_IDX")
+  @Column(length = CatalogConstants.IDENTIFIER_MAX_LENGTH)
+  private String user;
+
+  @Persistent
+  private String query;
+
+  public MQueryLog(String start, String user, String query) {
+    this.start   = start;
+    this.user    = user;
+    this.query   = query;
+  }
+
+  public String getStartTime() {
+    return start;
+  }
+
+  public String getUser() {
+    return user;
+  }
+
+  public String getQuery()   { return query;  }
+}
