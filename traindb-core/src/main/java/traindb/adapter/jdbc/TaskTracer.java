8d641ee2e7b13787deb440596cc5d3ce29d3092e
Taewhi Lee <taewhi@etri.re.kr>
2022-12-13 17:50:07 +0900

    fix: getExecutionTime

--- traindb-core/src/main/java/traindb/adapter/jdbc/TaskTracer.java
+++ traindb-core/src/main/java/traindb/adapter/jdbc/TaskTracer.java
@@ -1,3 +1,17 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package traindb.adapter.jdbc;
 
 import java.time.LocalDateTime;
@@ -8,95 +22,97 @@ import java.util.concurrent.TimeUnit;
 import traindb.catalog.pm.MTask;
 
 public class TaskTracer {
-    private long lastNanoTime;
-    int          idx;
-    List<MTask>  taskStatus;
+  int idx;
+  List<MTask> taskStatus;
+  private long lastNanoTime;
 
-    public TaskTracer() {
-        this.lastNanoTime = System.nanoTime();
-        this.idx = 0;
-        this.taskStatus = new ArrayList<MTask>();
+  public TaskTracer() {
+    this.lastNanoTime = System.nanoTime();
+    this.idx = 0;
+    this.taskStatus = new ArrayList<MTask>();
+  }
+
+  public static String getExecutionTime(long elapsed) {
+    long time_ms = TimeUnit.NANOSECONDS.toMicros(elapsed) % 1000000L;
+    long time_sec = TimeUnit.NANOSECONDS.toSeconds(elapsed) % 60;
+    long time_min = TimeUnit.NANOSECONDS.toMinutes(elapsed) % 60;
+    long time_hour = TimeUnit.NANOSECONDS.toHours(elapsed) % 24;
+    long time_days = TimeUnit.NANOSECONDS.toDays(elapsed);
+    String res = "";
+
+    if (time_days == 0) {
+      if (time_hour == 0) {
+        if (time_min == 0) {
+          res = String.format("                 %02d.%06d", time_sec, time_ms);
+        } else {
+          res = String.format("              %02d:%02d.%06d", time_min, time_sec, time_ms);
+        }
+      } else {
+        res = String.format("           %02d:%02d:%02d.%06d", time_hour, time_min, time_sec,
+            time_ms);
+      }
+    } else {
+      res = String.format("        %dd %02d:%02d:%02d.%06d", time_days, time_hour, time_min,
+          time_sec, time_ms);
     }
+    return res;
+  }
 
-    public List<MTask> getTaskLog() { return taskStatus;    }
+  public List<MTask> getTaskLog() {
+    return taskStatus;
+  }
 
-    public void startTaskTracer(String query) {
+  public void startTaskTracer(String query) {
 //        this.taskStatus.clear();
-        this.idx = 0;
+    this.idx = 0;
 
-        LocalDateTime now = LocalDateTime.now();
-        String taskTime = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS"));
+    LocalDateTime now = LocalDateTime.now();
+    String taskTime = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS"));
 
-        MTask newTask = new MTask(taskTime , this.idx, query, "FAIL");
-        this.taskStatus.add(newTask);
+    MTask newTask = new MTask(taskTime, this.idx, query, "FAIL");
+    this.taskStatus.add(newTask);
 
-        //printStatus( newTask, "startTaskTracer" );
-    }
+    //printStatus( newTask, "startTaskTracer" );
+  }
 
-    public void endTaskTracer() {
-        String taskStatus = "SUCCESS";
+  public void endTaskTracer() {
+    String taskStatus = "SUCCESS";
 
-        MTask currTask = this.taskStatus.get(0);
-        currTask.setStatus(taskStatus);
+    MTask currTask = this.taskStatus.get(0);
+    currTask.setStatus(taskStatus);
 
-        //printStatus( currTask, "endTaskTracer" );
-    }
+    //printStatus( currTask, "endTaskTracer" );
+  }
 
-    public void openTaskTime(String event) {
-        this.idx++;
-        this.lastNanoTime = System.nanoTime();
-        String taskStatus = "START";
+  public void openTaskTime(String event) {
+    this.idx++;
+    this.lastNanoTime = System.nanoTime();
+    String taskStatus = "START";
 
-        MTask newTask = new MTask("" , this.idx, event, taskStatus);
-        this.taskStatus.add(newTask);
+    MTask newTask = new MTask("", this.idx, event, taskStatus);
+    this.taskStatus.add(newTask);
 
-        //printStatus( newTask, "open task" );
-    }
+    //printStatus( newTask, "open task" );
+  }
 
-    public void closeTaskTime(String event) {
-        long newNanoTime = System.nanoTime();
-        long elapsed = newNanoTime - this.lastNanoTime;
-        String taskTime = getExecutionTime(elapsed);
+  public void closeTaskTime(String event) {
+    long newNanoTime = System.nanoTime();
+    long elapsed = newNanoTime - this.lastNanoTime;
+    String taskTime = getExecutionTime(elapsed);
 
-        MTask currTask = this.taskStatus.get(this.idx);
-        currTask.setTime(taskTime);
-        currTask.setStatus(event);
+    MTask currTask = this.taskStatus.get(this.idx);
+    currTask.setTime(taskTime);
+    currTask.setStatus(event);
 
-        //printStatus( currTask, "end task" );
-    }
+    //printStatus( currTask, "end task" );
+  }
 
-    public static String getExecutionTime(long elapsed)
-    {
-        long time_ms = TimeUnit.NANOSECONDS.toMicros(elapsed);
-        long time_sec = TimeUnit.NANOSECONDS.toSeconds(elapsed);
-        long time_min = TimeUnit.NANOSECONDS.toMinutes(elapsed);
-        long time_hour = TimeUnit.NANOSECONDS.toHours(elapsed);
-        long time_days = TimeUnit.MILLISECONDS.toDays(elapsed);
-        String res = "";
-
-        if (time_days == 0) {
-            if ( time_hour == 0 ) {
-                if ( time_min == 0 )
-                    res = String.format("                 %02d.%06d", time_sec, time_ms);
-                else
-                    res = String.format("              %02d:%02d.%06d", time_min, time_sec, time_ms);
-            }
-            else
-                res = String.format("           %02d:%02d:%02d.%06d", time_hour, time_min, time_sec, time_ms);
-        }
-        else {
-            res = String.format("        %dd %02d:%02d:%02d.%06d", time_days, time_hour, time_min, time_sec,time_ms);
-        }
-        return res;
-    }
-
-    public void printStatus(MTask currTask, String msg)
-    {
-        System.out.println("--------  " + msg + "   ----------");
-        System.out.println("time   = " + currTask.getTime());
-        System.out.println("idx    = " + currTask.getIdx());
-        System.out.println("task   = " + currTask.getTask());
-        System.out.println("status = " + currTask.getStatus());
-        System.out.println();
-    }
+  public void printStatus(MTask currTask, String msg) {
+    System.out.println("--------  " + msg + "   ----------");
+    System.out.println("time   = " + currTask.getTime());
+    System.out.println("idx    = " + currTask.getIdx());
+    System.out.println("task   = " + currTask.getTask());
+    System.out.println("status = " + currTask.getStatus());
+    System.out.println();
+  }
 }
