ede5c4576a19d288d4a24f8740e82ea33ff6b500
Taewhi Lee <taewhi@etri.re.kr>
2023-03-17 17:25:48 +0900

    add NIO library files

---
+++ traindb-core/src/main/java/traindb/engine/nio/Message.java
@@ -0,0 +1,120 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package traindb.engine.nio;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public final class Message {
+  private char type;
+  private byte[] body;
+  private ByteBuffer buf;
+
+  public Message(byte[] body) {
+    this(' ', body);
+  }
+
+  public Message(char type, byte[] body) {
+    this.type = type;
+    this.body = body;
+    this.buf = ByteBuffer.wrap(body);
+  }
+
+  public static Builder builder(char type) {
+    return new Builder(type);
+  }
+
+  public char getType() {
+    return type;
+  }
+
+  public byte[] getBody() {
+    return body;
+  }
+
+  public int peekInt() {
+    return buf.getInt(buf.position());
+  }
+
+  public char getChar() {
+    return (char) buf.get();
+  }
+
+  public short getShort() {
+    return buf.getShort();
+  }
+
+  public int getInt() {
+    return buf.getInt();
+  }
+
+  public byte[] getBytes(int length) {
+    byte[] bytes = new byte[length];
+    buf.get(bytes);
+    return bytes;
+  }
+
+  public String getCString() {
+    return ByteBuffers.getCString(buf);
+  }
+
+  public static final class Builder {
+    private char type;
+    private ByteBuffer buf;
+
+    public Builder(char type) {
+      this.type = type;
+      buf = ByteBuffer.allocate(1024);
+    }
+
+    public Builder putChar(char c) {
+      buf = ByteBuffers.enlargeByteBuffer(buf, ByteBuffers.BYTE_BYTES);
+      buf.put((byte) c);
+      return this;
+    }
+
+    public Builder putShort(short h) {
+      buf = ByteBuffers.enlargeByteBuffer(buf, ByteBuffers.SHORT_BYTES);
+      buf.putShort(h);
+      return this;
+    }
+
+    public Builder putInt(int i) {
+      buf = ByteBuffers.enlargeByteBuffer(buf, ByteBuffers.INTEGER_BYTES);
+      buf.putInt(i);
+      return this;
+    }
+
+    public Builder putBytes(byte[] bytes) {
+      buf = ByteBuffers.enlargeByteBuffer(buf, bytes.length);
+      buf.put(bytes);
+      return this;
+    }
+
+    public Builder putCString(String s) {
+      byte[] bytes = s.getBytes(StandardCharsets.UTF_8);
+      buf = ByteBuffers.enlargeByteBuffer(buf, bytes.length);
+      buf.put(bytes);
+      putChar('\0');
+      return this;
+    }
+
+    public Message build() {
+      byte[] body = Arrays.copyOf(buf.array(), buf.position());
+      return new Message(type, body);
+    }
+  }
+}
---
+++ traindb-core/src/main/java/traindb/engine/nio/MessageStream.java
@@ -0,0 +1,168 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package traindb.engine.nio;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+public final class MessageStream {
+  private static final int SEND_BUFFER_SIZE_DEFAULT = 8 * 1024;
+  private static final int RECV_BUFFER_SIZE_DEFAULT = 8 * 1024;
+
+  private final SocketChannel socketChannel;
+  private final ByteBuffer sendBuffer = ByteBuffer.allocate(SEND_BUFFER_SIZE_DEFAULT);
+  private final ByteBuffer recvBuffer = ByteBuffer.allocate(RECV_BUFFER_SIZE_DEFAULT);
+
+  public MessageStream(SocketChannel socketChannel) {
+    this.socketChannel = socketChannel;
+
+    // make receive buffer has no remaining elements
+    recvBuffer.flip();
+  }
+
+  public Message getInitialMessage() throws IOException {
+    byte[] body = getMessageBody();
+    return new Message(body);
+  }
+
+  public Message getMessage() throws IOException {
+    char type = getChar();
+    byte[] body = getMessageBody();
+    return new Message(type, body);
+  }
+
+  public void putMessage(Message message) throws IOException {
+    char type = message.getType();
+    putChar(type);
+    byte[] body = message.getBody();
+    putInt(ByteBuffers.INTEGER_BYTES + body.length);
+    putBytes(body);
+  }
+
+  // Flush sendBuffer so client will see buffered messages immediately
+  public void putMessageAndFlush(Message message) throws IOException {
+    putMessage(message);
+    flush();
+  }
+
+  private byte[] getMessageBody() throws IOException {
+    int len = getInt();
+    /* length count includes itself */
+    if (len < ByteBuffers.INTEGER_BYTES) {
+      throw new EOFException("invalid message length");
+    }
+    byte[] body = new byte[len - ByteBuffers.INTEGER_BYTES];
+    getBytes(body);
+    return body;
+  }
+
+  private char getChar() throws IOException {
+    if (recvBuffer.remaining() < ByteBuffers.BYTE_BYTES) {
+      read();
+    }
+
+    return (char) recvBuffer.get();
+  }
+
+  private int getInt() throws IOException {
+    if (recvBuffer.remaining() < ByteBuffers.INTEGER_BYTES) {
+      read();
+    }
+
+    return recvBuffer.getInt();
+  }
+
+  private void getBytes(byte[] bytes) throws IOException {
+    int pos = 0;
+    int len = bytes.length;
+    while (len > 0) {
+      if (!recvBuffer.hasRemaining()) {
+        read();
+      }
+
+      int amount = recvBuffer.remaining();
+      if (amount > len) {
+        amount = len;
+      }
+      recvBuffer.get(bytes, pos, amount);
+
+      pos += amount;
+      len -= amount;
+    }
+  }
+
+  private void putChar(char c) throws IOException {
+    if (sendBuffer.remaining() < ByteBuffers.BYTE_BYTES) {
+      flush();
+    }
+
+    sendBuffer.put((byte) c);
+  }
+
+  private void putInt(int i) throws IOException {
+    if (sendBuffer.remaining() < ByteBuffers.INTEGER_BYTES) {
+      flush();
+    }
+
+    sendBuffer.putInt(i);
+  }
+
+  private void putBytes(byte[] bytes) throws IOException {
+    int pos = 0;
+    int len = bytes.length;
+    while (len > 0) {
+      if (!sendBuffer.hasRemaining()) {
+        flush();
+      }
+
+      int amount = sendBuffer.remaining();
+      if (amount > len) {
+        amount = len;
+      }
+      sendBuffer.put(bytes, pos, amount);
+
+      pos += amount;
+      len -= amount;
+    }
+  }
+
+  private void read() throws IOException {
+    recvBuffer.compact();
+
+    /*
+     * It is guaranteed that if a channel is in blocking mode and
+     * there is at least one byte remaining in the buffer then
+     * read() will block until at least one byte is read.
+     */
+    int ret = socketChannel.read(recvBuffer);
+    if (ret == -1) {
+      throw new EOFException("unexpected EOF on client connection");
+    }
+
+    recvBuffer.flip();
+  }
+
+  public void flush() throws IOException {
+    sendBuffer.flip();
+
+    while (sendBuffer.hasRemaining()) {
+      socketChannel.write(sendBuffer);
+    }
+
+    sendBuffer.clear();
+  }
+}
